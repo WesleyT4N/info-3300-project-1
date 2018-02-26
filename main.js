e869ac455a6ab22cfeb6b5307a3536bec2d6e3f2
Clare Snyder <cds238@cornell.edu>
2018-02-25 23:53:26 -0500

    Added code to read words figure data
    see code following comment  //Read in words figure csv file.
    the code is untested but adapted from example in commented link

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Put any code here
 
+  //Read in words figure csv file.
+  //http://learnjsdata.com/read_data.html
+  d3.csv("/words_figure_data.csv", function(data) {
+    data.forEach(function(d) {
+      d.Avg_Daily_Visitors = +d.Avg_Daily_Visitors; //read this value as number, not string
+    });
+    return data;
+  });
+  
   // Part 1 Svg
   var worldMapSvg = d3.select("#view1Svg")
   .attr("preserveAspectRatio", "xMinYMin meet")
