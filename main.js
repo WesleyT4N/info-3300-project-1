e546b917f1ac044b5444142f808dd33db8b8521e
Wes T4N <wesleyt1998@gmail.com>
2018-02-27 00:46:37 -0500

    world map changes

--- main.js
+++ main.js
@@ -1,18 +1,86 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", () => {
-  // Put any code here
 
-  // Part 1 Svg
+
+
+function showVis1() {
   var worldMapSvg = d3.select("#view1Svg")
   .attr("preserveAspectRatio", "xMinYMin meet")
   .attr("viewBox", "0 0 600 290");
 
-  d3.json("/map_figure_data_with_coord.json", function (error, data) {
-    console.log(data);
-    for (var hostCountry of Object.entries(data)) {
-      console.log(hostCountry);
-    }
-  });
+  var projection = d3.geoNaturalEarth1();
+  var pathGenerator = d3.geoPath().projection(projection);
+
+  var countries;
+  var hostData;
+  var rawCountryData;
+  function showMap() {
+    projection.fitSize([600, 290],
+      countries);
+    pathGenerator = d3.geoPath().projection(projection);
+    var paths = worldMapSvg.selectAll("path.country")
+      .data(countries.features);
+    paths = paths.enter().append("path")
+      .attr("class", "country").merge(paths);
+    paths.attr("d", function (country) {
+      return pathGenerator(country);
+    }).style("fill", "#223144").style("stroke", "#111a25");
+    
+    console.log(countries);
+    var circles = worldMapSvg.selectAll("circle.capital")
+    .data(hostData);
+    circles = circles.enter().append("circle")
+      .attr("class", "capital")
+      .attr("r", "1")
+      .attr("cx", function (d) {
+        var point = [d.data.coordinates.longitude,
+          d.data.coordinates.latitude];
+        return projection(point)[0];
+      })
+      .attr("cy", function (d) {
+        var point = [d.data.coordinates.longitude,
+          d.data.coordinates.latitude];
+        return projection(point)[1];
+      })
+      .style("fill", function (d) {
+        return "#8abfff";
+      })
+      .merge(circles);
+    
+  }
+
+  function callback(error, data, topo) {
+    rawCountryData = topo;
+    console.log(topo);
+    hostData = data;
+    hostData = Object.keys(hostData).map(function(key) {
+      return { key, data : hostData[key]};
+    });
+    console.log(hostData);
+    countries = topojson.feature(topo, topo.objects.countries);
+    showMap();
+    // for (var hostCountry of Object.entries(data)) {
+    //   console.log(hostCountry);
+    // }
+  }
+
+
+  d3.queue()
+  .defer(d3.json, "/map_figure_data_with_coord.json")
+  .defer(d3.json, "/world-50m.json")
+  .await(callback);
+
+  
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Put any code here
+
+  // Part 1 Svg
+  showVis1();
+
 
 
 
